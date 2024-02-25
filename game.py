f509d6c6bcac9b91b118e532e3b5df6c91c08082
aminehochlef <hochamine22@gmail.com>
2024-02-25 19:48:54 +0100

    added background and Hangman status to the game

--- game.py
+++ game.py
@@ -1,19 +1,42 @@
 import pygame
 import os
 
+# Setup diplay
 pygame.init()
 WIDTH, HEIGHT = 800, 500
-pygame.display.set_mode((WIDTH, HEIGHT))
+win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Hangman - Game")
 
+# load images
+images = []
+for i in range(7):
+    image_path = os.path.join("images", "hangman" + str(i) + ".png")
+    image = pygame.image.load(image_path)
+    images.append(image)
+
+
+# Game variables
+hangman_status = 4
+
+# Colors
+WHITE = (255,255,255)
+
+# Setup game loop
 FPS = 60
 clock = pygame.time.Clock()
 run = True
 
 while run:
     clock.tick(FPS)
+    
+    win.fill(WHITE)
+    win.blit(images[hangman_status], (150,100))
+    pygame.display.update()
+        
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-            
+        if event.type == pygame.MOUSEBUTTONDOWN:
+             pos = pygame.mouse.get_pos()
+             print(pos)
 pygame.quit()
