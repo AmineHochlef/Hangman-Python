aca6591951b84566161968b830cf7428cf30dfba
aminehochlef <hochamine22@gmail.com>
2024-02-26 20:25:48 +0100

    configuring the visiblity of the letters

--- game.py
+++ game.py
@@ -29,7 +29,7 @@ A = 65
 for i in range(26):
     x = startx + GAP * 2 + (RADIUS * 2 + GAP) * (i % 13)
     y = starty + ((i // 13) * (GAP + RADIUS * 2))
-    letters.append([x, y, chr(A + i)])
+    letters.append([x, y, chr(A + i), True])
 
 # Game variables
 hangman_status = 4
@@ -47,10 +47,11 @@ def draw():
     win.fill(WHITE)
     # draw buttons
     for letter in letters:
-        x, y, ltr = letter
-        pygame.draw.circle(win, BLACK, (x, y), RADIUS, 3)
-        text = LETTER_FONT.render(ltr, 1, BLACK)
-        win.blit(text, (x - text.get_width() / 2 , y - text.get_height() / 2))
+        x, y, ltr, visible = letter
+        if visible:
+            pygame.draw.circle(win, BLACK, (x, y), RADIUS, 3)
+            text = LETTER_FONT.render(ltr, 1, BLACK)
+            win.blit(text, (x - text.get_width() / 2 , y - text.get_height() / 2))
         
         
     win.blit(images[hangman_status], (150, 100))
@@ -67,9 +68,10 @@ while run:
         if event.type == pygame.MOUSEBUTTONDOWN:
             m_x, m_y = pygame.mouse.get_pos()
             for letter in letters:
-                x, y, ltr = letter
-                dis = math.sqrt((x - m_x) ** 2 + (y - m_y) ** 2)
-                if dis < RADIUS:
-                    print(ltr)
+                x, y, ltr, visible = letter
+                if visible:
+                    dis = math.sqrt((x - m_x) ** 2 + (y - m_y) ** 2)
+                    if dis < RADIUS:
+                        letter[3] = False
 
 pygame.quit()
