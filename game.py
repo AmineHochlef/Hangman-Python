11a8ff00f7e51a642a0df165c704e5c553c18cbf
aminehochlef <hochamine22@gmail.com>
2024-02-26 19:46:52 +0100

    added necessary buttons for the game

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import pygame
 import os
 
-# Setup diplay
+# Setup display
 pygame.init()
 WIDTH, HEIGHT = 800, 500
 win = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -14,29 +14,48 @@ for i in range(7):
     image = pygame.image.load(image_path)
     images.append(image)
 
+# button variables
+RADIUS = 20
+GAP = 15
+letters = []
+startx = round((WIDTH - (RADIUS * 2 + GAP) * 13) / 2)
+starty = 400
+for i in range(26):
+    x = startx + GAP * 2 + (RADIUS * 2 + GAP) * (i % 13)
+    y = starty + ((i // 13) * (GAP + RADIUS * 2))
+    letters.append([x, y])
 
 # Game variables
 hangman_status = 4
 
 # Colors
-WHITE = (255,255,255)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
 # Setup game loop
 FPS = 60
 clock = pygame.time.Clock()
 run = True
 
+def draw():
+    win.fill(WHITE)
+    # draw buttons
+    for letter in letters:
+        x, y = letter
+        pygame.draw.circle(win, BLACK, (x, y), RADIUS, 3)
+    win.blit(images[hangman_status], (150, 100))
+    pygame.display.update()
+
 while run:
     clock.tick(FPS)
     
-    win.fill(WHITE)
-    win.blit(images[hangman_status], (150,100))
-    pygame.display.update()
-        
+    draw()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-             pos = pygame.mouse.get_pos()
-             print(pos)
+            pos = pygame.mouse.get_pos()
+            print(pos)
+
 pygame.quit()
